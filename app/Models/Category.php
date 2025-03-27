cd6a4b13f5a5b77bc576b1fcf0bbff5eb985b565
Yahya Poernama <yahyapoernama@gmail.com>
2025-03-27 16:23:34 +0700

    chore: Expand fillable fields in Category model to include slug and description

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,7 +12,7 @@ class Category extends Model
     protected $table = 'categories';
 
     protected $fillable = [
-        'name'
+        'name', 'slug', 'description'
     ];
 
     public function products()
