5430193a6b5bb33ecd89b7b16fce58e3f04f997f
Yahya Poernama <yahyapoernama@gmail.com>
2025-03-27 16:23:20 +0700

    fix: Update roles and categories migrations to remove uniqueness constraints on name and slug fields because SoftDeletes

--- database/migrations/2025_03_16_144220_create_roles_table.php
+++ database/migrations/2025_03_16_144220_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->uuid('created_by')->nullable()->index();
             $table->timestamps();
             $table->softDeletes();
--- database/migrations/2025_03_16_151053_create_categories_table.php
+++ database/migrations/2025_03_16_151053_create_categories_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('slug')->unique();
+            $table->string('name');
+            $table->string('slug');
             $table->text('description')->nullable();
             $table->uuid('created_by')->nullable()->index();
             $table->timestamps();
